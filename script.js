84482bd70647bf2730ceb4860df31fc35632075f
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2024-06-19 22:30:25 +0800

    修改动画速度,加载完后再调用动画

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ window.addEventListener('hashchange', () => {
     setTimeout(() => {
         loadViewFile(getHashParam());
         spacer.classList.remove('expanded');
-    }, 510);
+    }, 520);
 });
 
 // 初次加载时调用
