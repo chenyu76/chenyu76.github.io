a94d396c4afb5ede16920f197b53e0d818552cf3
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2024-08-21 21:49:52 +0800

    更新readme

--- program/TractorBattle3D/readme.md
+++ program/TractorBattle3D/readme.md
@@ -1,3 +1,7 @@
-Tractor Battle 3D
+# Tractor Battle 3D
 
-一个神秘的双人游戏，使用Godot完成
+一个神秘的双人游戏，使用Godot完成（其实还没有完全完成）
+
+音乐来自Godot的教程
+
+[开始](https://chenyu76.github.io/program/TractorBattle3D/tb.html)
--- skipping-classes-for-introduction-to-entrepreneurship-for-the-future/script.js
+++
@@ -1,8 +0,0 @@
-function updateText() {
-    var input1 = document.getElementById('input1').value;
-    var input2 = document.getElementById('input2').value;
-
-    document.getElementById('text1').textContent = input1;
-    document.getElementById('text2').textContent = input2;
-}
-
