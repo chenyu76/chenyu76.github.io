a7d576487858dcd3e26bf698641f8d8d32fd99d7
chenyu76 <chenyu76@users.noreply.github.com>
2024-09-05 13:41:46 +0000

    deploy: bd2886e907293cabe71069de9d5dac2a386598b2

--- toc.py
+++ toc.py
@@ -53,19 +53,19 @@
 │　　│</br>
 │　　├──skipping-classes-for-introduction-to-entrepreneurship-for-the-future<br/>
 │　　│　　│</br>
-│　　│　　├──<a href="program/skipping-classes-for-introduction-to-entrepreneurship-for-the-future/index.html"> index.html </a><br/>
+│　　│　　└──<a href="program/skipping-classes-for-introduction-to-entrepreneurship-for-the-future/index.html"> index.html </a><br/>
 │　　│</br>
 │　　├──TractorBattle3D<br/>
 │　　│　　│</br>
 │　　│　　├──<a href="program/TractorBattle3D/tb.html"> tb.html </a><br/>
 │　　│　　│</br>
-│　　│　　├──<a href="#program/TractorBattle3D/readme.md"> readme </a><br/>
+│　　│　　└──<a href="#program/TractorBattle3D/readme.md"> readme </a><br/>
 │　　│</br>
 │　　├──WheelOfFortune<br/>
 │　　│　　│</br>
 │　　│　　├──<a href="#program/WheelOfFortune/readme.md"> readme </a><br/>
 │　　│　　│</br>
-│　　│　　├──<a href="program/WheelOfFortune/WheelOfFortune.html"> WheelOfFortune.html </a><br/>
+│　　│　　└──<a href="program/WheelOfFortune/WheelOfFortune.html"> WheelOfFortune.html </a><br/>
 │　　│</br>
 │　　└──<a href="program/counter.html"> counter.html </a><br/>
 │</br>
@@ -91,14 +91,17 @@ def folder_tree(path, depth = 0, startpath = None, line_d = 0):
     items = [
         s for s in items if (os.path.isdir(os.path.join(path, s)) and (not s.startswith('.'))) or (os.path.isfile(os.path.join(path, s)) and any([s.endswith(x) for x in clickable_extension ])) 
     ]
+    # 排除掉 program 文件夹里的 js
+    items = [
+        i for i in items if not (i.split('.')[-1] == "js" and "program" in path)
+    ]
+
     def item_to_button():
             rpath = os.path.relpath((startpath if startpath else path), startpath) # 相对路径
             withSharp = False
             for ex in article_extension:  # 这些视为文章
                 if item.endswith(ex):
                     withSharp = True
-                    if ex == '.js' and ("program" in rpath): # js 文章需要排除指定地方的
-                        break
                     table.append(style + f'<a href="#{os.path.join(rpath, item)}"> {item.rstrip(ex)} </a><br/>')
                     break
             if not withSharp: # 其余仅为可点击项
