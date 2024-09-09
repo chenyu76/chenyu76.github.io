5c4ee260213f2cccb2d4f341556941d97c179d77
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2024-09-09 22:04:19 +0800

    修复文件名

--- toc.py
+++ toc.py
@@ -61,11 +61,11 @@ def folder_tree(path, depth = 0, startpath = None, line_d = 0):
 
 def generate_recommand():
     recommend = [
-        {"date": 20240909, "link": "./program/7-piece-puzzle/Readme and solution.md", "info": "一个拼图游戏"},
-        {"date": 202408, "link": "./program/TractorBattle3D/readme.md", "info": "生成障碍阻碍对手的多人本地游戏"},
-        {"date": 0, "link": "./摘抄/-随机一篇-.js", "info": "看看随机一篇摘抄"},
-        {"date": 202408, "link": "./文章/做了一些学校的LaTeX模板.js"},
-        {"date": 20240620, "link": "./文章/关于这个网站.md"},
+        {"date": 20240909, "link": "program/7-piece-puzzle/Readme and solution.md", "info": "一个拼图游戏"},
+        {"date": 202408, "link": "program/TractorBattle3D/readme.md", "info": "生成障碍阻碍对手的多人本地游戏"},
+        {"date": 0, "link": "摘抄/-随机一篇-.js", "info": "看看随机一篇摘抄"},
+        {"date": 202408, "link": "文章/做了一些学校的LaTeX模板.js"},
+        {"date": 20240620, "link": "文章/关于这个网站.md"},
     ]
 
     list = []
