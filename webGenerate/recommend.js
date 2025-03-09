2da1e0f9e185624154385f331124d40d5d246e4c
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2025-03-09 17:17:44 +0800

    主页显示更多详细信息

--- webGenerate/recommend.js
+++ webGenerate/recommend.js
@@ -1,11 +1,38 @@
 const recommend = [
-    {date: 20250206, link: "writings/new-year-new-theme.html", info: "蛇年的全新网站样式！动态背景！"},
-    {date: 20241215, link: "writings/julia-set.pdf", info: "Julia集的计算机艺术(.pdf)"},
-{date: 20240909, link: "program/7-piece-puzzle/Readme and solution.html", info: "一个拼图游戏"},
-{date: 202408, link: "program/TractorBattle3D/readme.html", info: "生成障碍阻碍对手的多人本地游戏"},
-//{date: 0, link: "摘抄/-随机一篇-.js", info: "看看随机一篇摘抄"},
-//{date: 202408, link: "文章/做了一些学校的LaTeX模板.js"},
-{date: 20240620, link: "writings/about-the-website.html"}
+  {
+    date: 20250206,
+    link: "writings/new-year-new-theme.html",
+    title: "新年新主题",
+    info: "蛇年的全新网站样式！动态随机生成的js背景！",
+  },
+  {
+    date: 20241215,
+    link: "writings/julia-set.pdf",
+    title: "Julia集的计算机艺术",
+    info: "(pdf文件) 简单的迭代居然能生成复杂而迷人的图形。（某个课程作业）",
+  },
+  {
+    date: 20240909,
+    link: "program/7-piece-puzzle/Readme and solution.html",
+    title: "一个拼图游戏",
+    info: "关卡随机生成的拼图游戏",
+  },
+  {
+    date: 202408,
+    link: "program/TractorBattle3D/readme.html",
+    title: "一个对抗游戏",
+    info: "生成障碍阻碍对手的多人本地游戏，Godot练习",
+  },
+  //{date: 0, link: "摘抄/-随机一篇-.js", info: "看看随机一篇摘抄"},
+  { date: 202408, link: "writings/some-szu-LaTeX-template.html",
+    title: "深大的一些LaTeX文档模板",
+    },
+  {
+    date: 20240620,
+    link: "writings/about-the-website.html",
+    title: "关于本站",
+    info: "第一篇文章",
+  },
 ];
 
 export default recommend;
