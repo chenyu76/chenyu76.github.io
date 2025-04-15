401ef16ecd6cec60296838fea51810fbdb2f9e6f
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2025-04-15 15:50:18 +0800

    remove useless code

--- script.js
+++ script.js
@@ -2,49 +2,6 @@ function the_script() {
   return 'This is the JavaScript file of this website. </br>See raw file at <a href="script.js"> script.js </a>';
 }
 
-// 获取当前的哈希值
-function getHashParam() {
-  return window.location.hash
-    ? (function (str) {
-        // 如果参数没有后缀就添加.md后缀
-        if (str) {
-          if (str.includes(".")) return str;
-          else return str + ".md";
-        }
-        return str;
-      })(window.location.hash.substring(1))
-    : "README.md";
-}
-
-// TODO: hash变化时跳转到指定位置（目录）
-// 监听 hashchange 事件以动态加载新的 Markdown 文件
-/* window.addEventListener("hashchange", async () => {
-  // 显示加载
-  document.getElementById("heading").innerHTML = "<h1>加载中…</h1>";
-  const spacer = document.getElementById("anime-spacer");
-  scrollToTop();
-  spacer.classList.remove("shrinked");
-
-  // 开始处理文本内容
-  var start = performance.now();
-  // 也许我应该使用 github action 将文本都处理好了直接下载就行而不是客户端处理
-  var tt = await parseFile(getHashParam());
-  var end = performance.now();
-
-  // 等待动画结束
-  setTimeout(
-    () => {
-      loadText(tt);
-      setTimeout(() => {
-        spacer.classList.add("shrinked");
-      }, 20);
-    },
-    ((a) => {
-      return a > 0 ? a : 0;
-    })(500 - (end - start)),
-  );
-}); */
-
 // 平滑滚动到顶部
 function scrollToTop() {
   document.body.scrollTo({
