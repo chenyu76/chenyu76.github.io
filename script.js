8f3974750274c2d841c9a7f1bec06025097c6195
chenyu76 <chenyu76@users.noreply.github.com>
2025-04-17 08:38:00 +0000

    deploy: 15fb96e37d0c90dcd90fb4b9d86df208b2ce646d

--- script.js
+++ script.js
@@ -2,50 +2,6 @@ function the_script() {
   return 'This is the JavaScript file of this website. </br>See raw file at <a href="script.js"> script.js </a>';
 }
 
-
-// 获取当前的哈希值
-function getHashParam() {
-  return window.location.hash
-    ? (function (str) {
-        // 如果参数没有后缀就添加.md后缀
-        if (str) {
-          if (str.includes(".")) return str;
-          else return str + ".md";
-        }
-        return str;
-      })(window.location.hash.substring(1))
-    : "README.md";
-}
-
-// TODO: hash变化时跳转到指定位置（目录）
-// 监听 hashchange 事件以动态加载新的 Markdown 文件
-/* window.addEventListener("hashchange", async () => {
-  // 显示加载
-  document.getElementById("heading").innerHTML = "<h1>加载中…</h1>";
-  const spacer = document.getElementById("anime-spacer");
-  scrollToTop();
-  spacer.classList.remove("shrinked");
-
-  // 开始处理文本内容
-  var start = performance.now();
-  // 也许我应该使用 github action 将文本都处理好了直接下载就行而不是客户端处理
-  var tt = await parseFile(getHashParam());
-  var end = performance.now();
-
-  // 等待动画结束
-  setTimeout(
-    () => {
-      loadText(tt);
-      setTimeout(() => {
-        spacer.classList.add("shrinked");
-      }, 20);
-    },
-    ((a) => {
-      return a > 0 ? a : 0;
-    })(500 - (end - start)),
-  );
-}); */
-
 // 平滑滚动到顶部
 function scrollToTop() {
   document.body.scrollTo({
@@ -54,7 +10,6 @@ function scrollToTop() {
     behavior: "smooth",
   });
 }
-
 
 // 是否是竖屏模式
 let pixelImgInit = () => {
@@ -82,3 +37,24 @@ let pixelImgInit = () => {
 pixelImgInit();
 // 每隔十分钟更新背景
 setInterval(pixelImgInit, 600000);
+
+// 处理窗口大小变化
+// 防抖函数封装
+function debounce(func, delay = 250) {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId); // 清除之前的定时器
+    timeoutId = setTimeout(() => {
+      func.apply(this, args); // 延迟执行目标函数
+    }, delay);
+  };
+}
+// 处理窗口大小变化
+function handleResize() {
+  is_first_img_init = true;
+  pixelImgInit();
+}
+// 使用防抖包装处理函数
+const debouncedResize = debounce(handleResize, 300);
+// 监听窗口resize事件
+window.addEventListener("resize", debouncedResize);
