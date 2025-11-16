9b10b94f43f4e5e4b7a98a61611c42ef0aea6b58
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2025-11-17 00:20:47 +0800

    修改.sh脚本为.hs脚本

---
+++ quick-references/LaTeX/TikZ/TikZ2svg.hs
@@ -0,0 +1,57 @@
+#!/usr/bin/env runhaskell
+
+import Control.Monad (unless)
+import Data.Functor ((<&>))
+import System.Exit (ExitCode (..), exitWith)
+import System.IO (hPutStrLn, stderr)
+import System.Process (readProcess, readProcessWithExitCode)
+
+startWith :: String -> String -> Bool
+startWith str prefix = take (length prefix) str == prefix
+
+main :: IO ()
+main = do
+  -- 检查依赖
+  xelatexExists <- readProcess "which" ["xelatex"] "" Data.Functor.<&> (not . null)
+  inkscapeExists <- readProcess "which" ["inkscape"] "" Data.Functor.<&> (not . null)
+  unless (xelatexExists && inkscapeExists) $ do
+    hPutStrLn stderr "错误：缺少必要依赖 xelatex"
+    exitWith (ExitFailure 1)
+
+  -- 从stdin读取
+  input <- getContents
+
+  let texContent
+        | input `startWith` "\\documentclass" = input
+        | input `startWith` "\\begin{document}" =
+            unlines
+              [ "\\documentclass{standalone}",
+                "\\usepackage{tikz}",
+                "\\usepackage{xcolor}",
+                "\\usetikzlibrary{arrows.meta}",
+                input
+              ]
+        | otherwise =
+            unlines
+              [ "\\documentclass{standalone}",
+                "\\usepackage{tikz}",
+                "\\usepackage{xcolor}",
+                "\\usetikzlibrary{arrows.meta}",
+                "\\begin{document}",
+                "\\begin{tikzpicture}",
+                input,
+                "\\end{tikzpicture}",
+                "\\end{document}"
+              ]
+
+  -- 直接使用shell命令（类似Bash的方式）
+  writeFile "/tmp/diagram.tex" texContent
+  (exitCode, _, _) <-
+    readProcessWithExitCode
+      "sh"
+      ["-c", "cd /tmp && xelatex -interaction=nonstopmode diagram.tex && inkscape --export-type=svg --export-plain-svg=diagram.svg diagram.pdf"]
+      ""
+
+  case exitCode of
+    ExitSuccess -> readFile "/tmp/diagram.svg" >>= putStr
+    _ -> hPutStrLn stderr "编译失败！" >> exitWith (ExitFailure 1)
