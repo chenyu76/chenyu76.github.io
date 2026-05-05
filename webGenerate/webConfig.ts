cf50a3b49e67f1472d6e0c4b7a9bfe8e03562efd
chenyu76 <51879608+chenyu76@users.noreply.github.com>
2026-05-05 12:54:17 +0800

    fix comment

--- webGenerate/webConfig.ts
+++ webGenerate/webConfig.ts
@@ -29,19 +29,19 @@ export const gitRepositories = {
  * date:
  * 日期，格式YYYYMMDD 或 YYYYMM 或 YYYY 或
  * 字符串（这会移交给JavaScript的Date对象处理）
- * TODO:
- * 最好能直接从markdown文件中读出来，因为我在markdown中保持在最后一行写日期的惯例。
+ * 如果不指定，会尝试从md文件最后一行读出来
  *
  * title:
  * 标题（可选，若不填，尝试从对应的markdown文件的第一行寻找标题）
  *
  * info:
  * html格式的介绍
- * （可选，若不填，
- * TODO:
- * 我希望能从生成的html文件里找到第一个<p>填进去，但目前还没实现）
+ * （可选，若不填，从生成的html文件里找到第一个<p>填进去）
  */
 export const recommend = [
+  {
+    link: "writings/how-to-expose-ssh-to-the-internet-with-frp/index.html",
+  },
   {
     date: 20260401,
     link: "program/4d-sokoban/index.html",
